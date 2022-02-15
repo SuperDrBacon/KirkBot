afb93a90e0c192bd066c89bad3a2c459a95878fa
SDB <1game2joys@gmail.com>
2022-02-15 21:30:32 +0100

    add json to txt cleaned

--- cogs/log.py
+++ cogs/log.py
@@ -6,12 +6,12 @@ import typing
 import asyncio
 import json
 import os
-import pandas as pd
+import re
 from discord.ui import Button, View
 from discord.ext import commands
 
 jsonpath = os.path.abspath(os.getcwd()) + "/cogs/log.json"
-
+messagestxt = os.path.abspath(os.getcwd()) + "/messages.txt"
 
 init = {
     "servers":[{
@@ -122,8 +122,67 @@ class logger(commands.Cog):
         
         with open(jsonpath, 'w') as fout:
             json.dump(init, fout, indent = 4)
+            
+    @commands.has_permissions(administrator=True)      
+    @commands.command(aliases=["up"])
+    async def update_mesages(self, ctx):
+        iserver = 0
+        ichannel = 0
+        imessage = 0
+        before = time.monotonic_ns()
+        with open(jsonpath, 'r') as fin:
+            file_data = json.load(fin)
+            with open(messagestxt, 'w', encoding='utf-8') as messagein:
+                for servers in file_data["servers"]:
+                    if servers["serverID"] == 123 or servers["serverID"] == 937056927312150599:
+                        continue
+                    iserver += 1
+                    for channels in servers["channels"]:
+                        if channels["channelID"] == 939083691790061601 or channels["channelID"] ==939221538949980210:
+                            ichannel += 1
+                            for message in channels["messages"]:
+                                text = str(message["message"])
+                                
+                                if text == "[]":
+                                    continue
+                                
+                                emojipattern = r'<.*?>'
+                                text2 = re.sub(emojipattern, '', text)
 
-
+                                commandpluspattern = r'\+([\S]+.)'
+                                text3 = re.sub(commandpluspattern, '', text2)
+                                
+                                links = r'(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,10}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)'
+                                text4 = re.sub(links, '', text3)
+                                
+                                commanddotpattern = r'\.([\S]+.)'
+                                text5 = re.sub(commanddotpattern, '', text4)
+                                
+                                commandcommapattern = r'\,([\S]+.)'
+                                text6 = re.sub(commanddotpattern, '', text5)
+                                
+                                leadingspacespattern = r'^ +'
+                                text7 = re.sub(leadingspacespattern, '', text6)
+                                
+                                exclamationpattern = r'\!([\S]+.)'
+                                text8 = re.sub(exclamationpattern, '', text7)
+                                
+                                atpattern = r'\@([\S]+.)'
+                                text9 = re.sub(atpattern, '', text8)
+                                
+                                # newline = '\n'
+                                # text10 = re.sub(newline, ' ', text9)
+                                
+                                if not text9:
+                                    continue
+                                
+                                messagein.write(text9+'\n')
+                                imessage += 1
+        after = (time.monotonic_ns() - before) / 1000000000
+        await ctx.send(f'{after}s')
+        await ctx.send(f'servers: {iserver} channels: {ichannel} messages: {imessage}')
+            
+            
 def setup(bot):
     bot.add_cog(logger(bot))
 
