198ca292ba5edf4a78625929809ea008007dbab9
SDB <1game2joys@gmail.com>
2022-03-14 21:23:51 +0100

    +connect. disconnect

--- KirkBot.py
+++ KirkBot.py
@@ -14,18 +14,28 @@ intents = discord.Intents().all()
 
 bot = commands.Bot(command_prefix=config['BOTCONFIG']['prefix'], help_command=None, case_insensitive=True, intents=intents)
 
-@bot.event
-async def on_ready():
-    print(f'{title} main online')
-
-
 for filename in os.listdir(path + "\\cogs"):
     if filename.endswith('.py'):
         name = filename[:-3]
-        bot.load_extension(f"cogs.{name}")
+        try:
+            bot.load_extension(f"cogs.{name}")
+        except Exception as e:
+            print(f"Error when loading module because: {e}")
 
 try:
     bot.run(config['BOTCONFIG']['token'])
 except Exception as e:
     print(f"Error when logging in: {e}")
 
+
+@bot.event
+async def on_ready():
+    print(f'{title} main online')
+
+@bot.event
+async def on_connect():
+    print(f'{title} connected successfully.')
+
+@bot.event
+async def on_disconnect():
+    print(f'{title} disconnected.')
