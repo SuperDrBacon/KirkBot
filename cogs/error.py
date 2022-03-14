f47638d1410e43d6f6b799bd1d55b8bb62f1eb41
SDB <1game2joys@gmail.com>
2022-03-14 21:23:01 +0100

    moved error to own module

---
+++ cogs/error.py
@@ -0,0 +1,35 @@
+import discord
+import asyncio
+import os
+import traceback
+from discord.ext import commands
+
+class Error(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Error module online')
+    
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        error = getattr(error, "original", error)
+
+        if isinstance(error, (discord.Forbidden, commands.NoPrivateMessage)):
+            await ctx.send("I couldn't send this information to you via direct message. Are your DMs enabled?")
+            
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(f"You missed the argument `{error.param.name}` for this command!")
+            
+        elif isinstance(error, commands.UserInputError):
+            await ctx.send(f"I can't understand this command message! Please check `.,help {ctx.command}`")
+            
+        elif isinstance(error, commands.CommandNotFound):
+            await ctx.send(f"No such command found. Try `.,help`")
+            
+        else:
+            await ctx.send("The command you've entered could not be completed at this time.")
+
+def setup(bot):
+    bot.add_cog(Error(bot))
