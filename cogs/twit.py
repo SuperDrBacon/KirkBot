692935a1e7ebde37bb3659b64ad739ae569079c4
SDB <1game2joys@gmail.com>
2022-05-03 22:35:28 +0200

    first test

--- cogs/twit.py
+++ cogs/twit.py
@@ -1,7 +1,7 @@
 import os
 import discord
 import tweepy
-from discord.ext import commands
+from discord.ext import commands, tasks
 from configparser import ConfigParser
 
 
@@ -34,11 +34,39 @@ class Twitter(commands.Cog):
     async def on_message(self, ctx):
         pass
     
+    
     @commands.has_permissions(administrator=True)
     @commands.group(aliases=["tweet"], invoke_without_command=True)
-    async def twittercommand(self, ctx):
+    async def tweet_base(self, ctx):
         
         pass
+    
+    @tweet_base.command(name="currentstream", invoke_without_command=True)
+    async def tweet_currentstream(self, ctx):
+        pass
+    
+    @commands.has_permissions(administrator=True)
+    @commands.command(name="gettweets", aliases=["gt"])
+    async def twittercommand(self, ctx, interval: int = 10, *, input: str = None):
+            self.get_tweet_stream.start(input=input)
+            self.get_tweet_stream.change_interval = interval
+            pass
+    
+    
+    @tasks.loop(minutes=10)
+    async def get_tweet_stream(self, input):
+        await self.send(input)
+        
+        pass
+    
+    @get_tweet_stream.before_loop
+    async def before_get_tweet_stream(self):
+        print('Waiting to start get_tweet_stream')
+        await self.bot.wait_until_ready()
+        print('get_tweet_stream started')
+        
+    def cog_unload(self):
+        self.get_tweet_stream.cancel()
 
 def setup(bot):
     bot.add_cog(Twitter(bot))
