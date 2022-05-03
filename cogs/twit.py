84b62a2ae3c00edeb62ac7cf400b2914535b6b0f
SDB <1game2joys@gmail.com>
2022-05-03 23:56:17 +0200

    current active and stop

--- cogs/twit.py
+++ cogs/twit.py
@@ -36,31 +36,36 @@ class Twitter(commands.Cog):
         pass
     
     
-    @commands.has_permissions(administrator=True)
+    #@commands.has_permissions(administrator=True)
     @commands.group(aliases=["tweet"], invoke_without_command=True)
     async def tweet_base(self, ctx):
-        
-        pass
+        await ctx.send('refer to help command for more info')
+        #pass
     
     @tweet_base.command(name="currentstream", invoke_without_command=True)
     async def tweet_currentstream(self, ctx):
-        pass
+        current = self.get_tweet_stream.get_task()
+        await ctx.send(current)
+        #pass
     
     @commands.has_permissions(administrator=True)
     @tweet_base.command(name="gettweets", aliases=["gt"], invoke_without_command=True)
-    async def twittercommand(self, ctx, interval: int, *, input: str = None):
-            channelid = ctx.channel.id
+    async def twitter_command(self, ctx, interval: int = 10, *, input: str = None):
             self.get_tweet_stream.start(input=input, ctx=ctx)
-            self.get_tweet_stream.change_interval = interval
-            pass
+            self.get_tweet_stream.change_interval(seconds=interval)
+            #pass
+    @commands.has_permissions(administrator=True)
+    @tweet_base.command(name="stopstream", aliases=["st"], invoke_without_command=True)
+    async def stop_stream(self, ctx):
+        self.get_tweet_stream.cancel()
+        await ctx.send('stream stopped')
+        #pass
     
-    
-    @tasks.loop(minutes=10)
+    @tasks.loop(reconnect=True)
     async def get_tweet_stream(self, input, ctx):
-        #channel = self.bot.get_channel(channelid)
         await ctx.send(input)
         
-        pass
+        #pass
     
     @get_tweet_stream.before_loop
     async def before_get_tweet_stream(self):
