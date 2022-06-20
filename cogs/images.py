f408db2592853bf598486f78f3a0e164ab4cc464
SDB-RASPI <barrymeelzak@gmail.com>
2022-06-20 04:05:02 +0200

    Working text scaling

--- cogs/images.py
+++ cogs/images.py
@@ -47,27 +47,32 @@ class Images(commands.Cog):
             
             img_width = 500
             reac_width, reac_height = reac_img.size
-            new_scale = 1 - (abs((img_width-reac_width)/reac_width))
+            print(reac_width)
+            new_scale = 1 + ((img_width-reac_width)/reac_width)
             sb_width, sb_height = speech_bubble.size
-            img_text_height = 200
+            # img_text_height = 200
+            print(new_scale)
+            new_reac_height = round(reac_height * new_scale)
             speech_bubble = speech_bubble.resize((img_width, sb_height))
-            reac_img = reac_img.resize((img_width, reac_height*new_scale))
+            reac_img = reac_img.resize((img_width, new_reac_height))
             
             reacted_to_user = ctx.message.reference.resolved.author.name 
             reacted_to_message = ctx.message.reference.resolved.content
             text_for_img = f'{reacted_to_user}: {reacted_to_message}'
             fontw, fonth = font.getsize(text_for_img)
-            lines = textwrap.wrap(text_for_img, width=20)
+            lines = textwrap.wrap(text_for_img, width=40)
+            # print(lines)
             y_offset = (len(lines)*fonth)/2
+            img_text_height = fonth * len(lines) + 50
             y_text = (img_text_height/2)-(fonth/2) - y_offset
+            text_img = Image.new('RGB', (img_width, img_text_height), (255, 255, 255))
             for line in lines:
                 linew, lineh = font.getsize(line)
-                text_img = Image.new('RGB', (img_width, img_text_height), (255, 255, 255))
                 draw = ImageDraw.Draw(text_img)
-                draw.text(((img_width/2)-(fontw/2), y_text), text_for_img, (0, 0, 0), font=font)
+                draw.text(((img_width/2)-(linew/2), y_text), line, (0, 0, 0), font=font)
                 y_text += lineh
             
-            final_img = Image.new('RGB', (img_width, img_text_height+sb_height+reac_height))
+            final_img = Image.new('RGB', (img_width, img_text_height+sb_height+new_reac_height))
             final_img.paste(text_img, (0, 0))
             final_img.paste(speech_bubble, (0, img_text_height))
             final_img.paste(reac_img, (0, img_text_height+sb_height))
