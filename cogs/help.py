ca35c7b32e31dba6fcfe585bc17584f8898b0130
SDB-PC <1game2joys@gmail.com>
2022-08-09 16:07:00 +0200

    Added gcp explanaition and fixed error

--- cogs/help.py
+++ cogs/help.py
@@ -22,7 +22,7 @@ class Help(commands.Cog):
         embed = Embed(title='Help commands per group', color=discord.Colour.gold(), timestamp=timestamp)
         embed.add_field(name="Fun", value="`.,ping`\n`.,8ball`\n`.,checkem` `.,check` `.,c`\n`.,bigletter` `.,em`\n`.,braille` `.,br`\n`.,youtube` `.,yt`\n`.,tag` `.,tag get`\n`.,gcp` `.,gcp full`", inline=True)
         embed.add_field(name="AI", value="`.,ai`", inline=True) #\n`@David Marcus`
-        embed.add_field(name="Admin", value="`.,botstatus`\n`.,botststus set`", inline=True)
+        embed.add_field(name="Admin", value="`.,botstatus`\n`.,botstatus set`", inline=True)
         embed.add_field(name="Moderator", value="`No commands lmao`", inline=True)
         embed.add_field(name='―――――――――――――――――――', value='Use .,help [group] to see command aliases and descriptions', inline=False)
         embed.set_footer(text=botversion)
@@ -32,17 +32,23 @@ class Help(commands.Cog):
     async def help_fun(self, ctx,):
         embed = Embed(title='Help -> Fun', color=discord.Colour.green(), timestamp=timestamp)
         embed.add_field(name="Commands and descriptions", 
-                        value="`.,8ball` Ask the 8ball your burning questions\n`.,checkem, .,check, .,c` Generate random number and maybe check those dubs\n" +
-                        "`.,bigletter, .,em` Convert your message into emoji letters\n`.,braille, .,br` Convert your message into braille so blind people can read it\n" +
-                        "`.,youtube, .,yt` Input a search and get the first Youtube result back\n`.,tag, .,tag get` Tag another person in the server when you're tagged! Or see who is currently tagged")
+                        value="`.,8ball` Ask the 8ball your burning questions\n"+
+                            "`.,checkem, .,check, .,c` Generate random number and maybe check those dubs\n"+
+                            "`.,bigletter, .,em` Convert your message into emoji letters\n"+
+                            "`.,braille, .,br` Convert your message into braille so blind people can read it\n"+
+                            "`.,youtube, .,yt` Input a search and get the first Youtube result back\n"+
+                            "`.,tag, .,tag get` Tag another person in the server when you're tagged! Or see who is currently tagged\n"+
+                            "`.,gcp, .,gcp full` Get the current position of the Global Consciousness Project Dot. Use full to get an explanation of the different colours")
         embed.set_footer(text=botversion)
         await ctx.channel.send(embed=embed,)
         
     @help_base.command(name='ai', invoke_without_command=True)
     async def help_AI(self, ctx,):
         embed = Embed(title='Help -> AI', color=discord.Colour.blue(), timestamp=timestamp)
-        embed.add_field(name="Commands and descriptions", value="`.,ai` Talk to the GPT-3 bot and get a response\n`@David Marcus` Uses the custom AI model created from chat messages\n"+
-                        "`Reply to the bot` Replying to the bot will take previous messages into account when generating a response")
+        embed.add_field(name="Commands and descriptions", 
+                        value="`.,ai` Talk to the GPT-3 bot and get a response\n"+
+                            "`@David Marcus` Uses the custom AI model created from chat messages\n"+
+                            "`Reply to the bot` Replying to the bot will take previous messages into account when generating a response")
         embed.set_footer(text=botversion)
         await ctx.channel.send(embed=embed,)
         
@@ -50,7 +56,9 @@ class Help(commands.Cog):
     @commands.has_permissions(administrator=True)
     async def help_admin(self, ctx):
         embed = Embed(title='Help -> Admin', color=discord.Colour.red(), timestamp=timestamp)
-        embed.add_field(name="Commands and descriptions", value="`.,botstatus` See what the current bot status is\n`.,botstatus set [message]` Set the bot status to something else\n")
+        embed.add_field(name="Commands and descriptions", 
+                        value="`.,botstatus` See what the current bot status is\n"+
+                            "`.,botstatus set [message]` Set the bot status to something else")
         embed.set_footer(text=botversion)
         await ctx.channel.send(embed=embed)
 
