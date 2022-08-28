b791fad800ff6a24e5dce598a9252c4a108b15bc
SDB-PC <1game2joys@gmail.com>
2022-08-28 23:16:01 +0200

    Delete after delay to lessen spam

--- cogs/error.py
+++ cogs/error.py
@@ -4,6 +4,8 @@ import os
 import traceback
 from discord.ext import commands
 
+MSG_DEL_DELAY = 5
+
 class Error(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -18,21 +20,27 @@ class Error(commands.Cog):
 
         if isinstance(error, (discord.Forbidden, commands.NoPrivateMessage)):
             await ctx.send("I couldn't send this information to you via direct message. Are your DMs enabled?")
+            await ctx.message.delete(delay=MSG_DEL_DELAY)
             
         elif isinstance(error, commands.MissingRequiredArgument):
             await ctx.send(f"You missed the argument `{error.param.name}` for this command!")
+            await ctx.message.delete(delay=MSG_DEL_DELAY)
             
         elif isinstance(error, commands.UserInputError):
             await ctx.send(f"I can't understand this command message! Please check `.,help {ctx.command}`")
+            await ctx.message.delete(delay=MSG_DEL_DELAY)
             
         elif isinstance(error, commands.CommandNotFound):
             await ctx.send(f"No such command found. Try `.,help`")
+            await ctx.message.delete(delay=MSG_DEL_DELAY)
             
         elif isinstance(error, commands.CommandOnCooldown):
             await ctx.send(f"This command is on cooldown retard. Try again in {error.retry_after:.2f} seconds.")
+            await ctx.message.delete(delay=MSG_DEL_DELAY)
         
         elif isinstance(error, commands.CommandError):
             await ctx.send("The command you've entered could not be completed at this time.")
+            await ctx.message.delete(delay=MSG_DEL_DELAY)
             print(f'CONSOLE ONLY, COMMAND FAILED: {error}')        
         else:            
             print(f'CONSOLE ONLY, ALL FAILED: {error}')
