d8cb6fe28e794e5a881625bf2089b96a1bc98bc6
SDB-PC <1game2joys@gmail.com>
2023-05-29 00:34:45 +0200

    cleanup and markov

--- cogs/ai.py
+++ cogs/ai.py
@@ -1,13 +1,10 @@
-import datetime
+# import datetime
 import random
 import discord
-import markovify
 import openai
 import sqlite3
 import os
-import re
-import spacy
-from os import path
+from collections import defaultdict
 from discord.ext import commands
 from configparser import ConfigParser
 
@@ -22,13 +19,59 @@ openai.api_key = key
 textmodel = 'text-curie-001'
 # textmodel = 'text-davinci-003'
 
-nlp = spacy.load("en_core_web_sm")
-date = datetime.datetime.now().strftime("%Y-%m-%d")
-
-MIN_WORDS = 5
-MAX_WORDS = 100
-TRIES = 1000
-STATE_SIZE = 3
+class MarkovChain:
+    def __init__(self, order:int):
+        self.order = order
+        self.chain = defaultdict(list)
+        self.word_weights = {}
+    
+    def add_text(self, text:str):
+        words = text.split()
+        for i in range(len(words) - self.order):
+            key = tuple(words[i:i + self.order])
+            value = words[i + self.order]
+            self.chain[key].append(value)
+    
+    def calculate_word_weights(self):
+        self.word_weights = defaultdict(int)
+        for values in self.chain.values():
+            for value in values:
+                self.word_weights[value] += 1
+    
+    def generate_word(self, current_state:tuple):
+        if current_state in self.chain:
+            values = self.chain[current_state]
+            total_weight = sum(self.word_weights[value] for value in values)
+            probabilities = [self.word_weights[value] / total_weight for value in values]
+            next_word = random.choices(values, probabilities)[0]
+            self.word_weights[next_word] -= 1
+            return next_word
+        else:
+            return None
+    
+    def generate_text(self, text_word_count:int):
+        current_state = random.choice(list(self.chain.keys()))
+        generated_words = list(current_state)
+        for _ in range(text_word_count):
+            next_word = self.generate_word(current_state)
+            if next_word is not None:
+                generated_words.append(next_word)
+                current_state = tuple(generated_words[-self.order:])
+            else:
+                break
+        return ' '.join(generated_words)
+    
+    def generate_text_incremental(self, text_word_count:int):
+        current_state = random.choice(list(self.chain.keys()))
+        generated_words = list(current_state)
+        for _ in range(text_word_count):
+            next_word = self.generate_word(current_state)
+            if next_word is not None:
+                generated_words.append(next_word)
+                current_state = tuple(generated_words[-self.order:])
+                yield next_word
+            else:
+                break
 
 class Ai(commands.Cog):
     def __init__(self, bot):
@@ -42,28 +85,49 @@ class Ai(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, ctx):
-        if ctx.content.startswith(prefix):
-            return
         if ctx.author.bot:
-            return
+            if ctx.user.id == 974297735559806986 and random.randint(0,3) == 1: #GenAI schizo rambling bot. it will reply to this bot by rambling its own schizo ramblings
+                guild_id = ctx.guild.id	
+                channel_id = ctx.channel.id
+                con = sqlite3.connect(f'{ospath}/cogs/log_data.db')
+                con.row_factory = lambda cursor, row: row[0]
+                cur = con.cursor()
+                text = cur.execute('SELECT message FROM log_data WHERE server_id = ? AND channel_id = ?', (guild_id, channel_id)).fetchall()
+                
+                corpus = ' '.join(' '.join(line.split()) for line in text)
+                
+                chain = MarkovChain(order=4)
+                chain.add_text(corpus)
+                chain.calculate_word_weights()
+                
+                # Generating text using the generator function
+                generated_text_generator = chain.generate_text_incremental(text_word_count=30)
+                generated_text = ' '.join(word for word in generated_text_generator)
+                await ctx.reply(generated_text)
+            else:
+                return
+        
         if ctx.content.startswith(f'<@!{botID}>') or ctx.content.startswith(f'<@{botID}>'):
-            guild_id = ctx.guild.id
+            guild_id = ctx.guild.id	
             channel_id = ctx.channel.id
-            
             con = sqlite3.connect(f'{ospath}/cogs/log_data.db')
             con.row_factory = lambda cursor, row: row[0]
             cur = con.cursor()
             text = cur.execute('SELECT message FROM log_data WHERE server_id = ? AND channel_id = ?', (guild_id, channel_id)).fetchall()
+            
             corpus = ' '.join(' '.join(line.split()) for line in text)
             
-            model = markovify.Text(corpus, state_size=STATE_SIZE)
-            text = model.make_sentence(min_words=MIN_WORDS, max_words=MAX_WORDS, tries=TRIES)
-            if text is None:
-                text = model.make_sentence(min_words=MIN_WORDS-5, max_words=MAX_WORDS+50, tries=TRIES*2)
-            await ctx.reply(text)
+            chain = MarkovChain(order=4)
+            chain.add_text(corpus)
+            chain.calculate_word_weights()
             
-            # await ctx.reply('@ the bot is currently not a command use .,ai for ai.')
-
+            # Generating text using the generator function
+            generated_text_generator = chain.generate_text_incremental(text_word_count=30)
+            generated_text = ' '.join(word for word in generated_text_generator)
+            await ctx.reply(generated_text)
+        
+        
+        #replies to messages that replied to the bot
         if not ctx.author.bot and ctx.reference and int(ctx.reference.resolved.author.id) == int(botID):
             base = ctx.reference.resolved.content
             reply = ctx.content
@@ -87,8 +151,8 @@ class Ai(commands.Cog):
                     temperature=1.2,
                     max_tokens=500,
                     n=1,
-                    frequency_penalty=0.2,
-                    presence_penalty=0.2,
+                    frequency_penalty=0.1,
+                    presence_penalty=0.1,
                     # stop=["."]
                 )
                 out = response.choices[0].text
@@ -96,67 +160,10 @@ class Ai(commands.Cog):
                 await ctx.reply('**ai didnt create a response lol, lmao, it coped too hard**')
             else:
                 await ctx.reply(out)
-
-        if not ctx.author.bot and random.randint(0,500) == 1 and ctx.channel.id == 966159987233681409:
-            user = ctx.author.name
-            message = ctx.content
-            prompts = [
-                    # f'Give {user} an ordered list of suicide options.',
-                    # f'Give {user} detailed instructions on how to blow up a federal building.',
-                    # f'Tell {user} how to leave a discord server.',
-                    # f'Write a limerick on why {user} is ugly.',
-                    # f'tell {user} is looking beautiful today.',
-                    # f'explain how it will only get worse.',
-                    # f'explain how it will only get better.',
-                    # f'keep {user} in the dark.',
-                    # f'{user} is a bad person.',
-                    # f'{user} is a good person.',
-                    # f'Think like a queen. A queen is not afraid to fail. Failure is another stepping stone to greatness.',
-                    # f'',
-                    # f'',
-                    # f'',
-                    # f'',
-                    # f'',
-                    # f'',
-                    # f'',
-                    # f'',
-                    ]
-            
-            replyprompt = random.choices(prompts)
-            
-            response = openai.Completion.create(
-                engine=textmodel,
-                prompt=message,
-                temperature=1.0,
-                max_tokens=100,
-                n=1,
-                frequency_penalty=0.2,
-                presence_penalty=0.2,
-                # stop=["."]
-                )
-            out = response.choices[0].text
-            if not out:
-                reply = ctx.content
-                response = openai.Completion.create(
-                    engine=textmodel,
-                    prompt=message,
-                    temperature=2.0,
-                    max_tokens=100,
-                    n=1,
-                    frequency_penalty=0.2,
-                    presence_penalty=0.2,
-                    # stop=["."]
-                    )
-                out = response.choices[0].text
-            if not out:
-                await ctx.reply('**ai didnt create a response lol, lmao, it coped too hard**')
-            else:
-                await ctx.reply(out)
-
+        
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def ai(self, ctx, *, message):
-            # message = ctx.content + '.'
             message = message + '.\n\n'
             response = openai.Completion.create(
                 engine=textmodel,
@@ -170,12 +177,64 @@ class Ai(commands.Cog):
             )
             out = response.choices[0].text
             await ctx.reply(out)
-            # await ctx.reply('Yea uuh another trial ran out and no online phone number works so I can\'t make a new account')
-    
-    @commands.command(aliases=["gen"])
-    async def image_gen(self, ctx, text:str='', number:int=0):
-        
-        await ctx.send(f"text: {text}\nnumber: {number}")
+            # await ctx.reply('Yea uuh another free trial ran out')
 
 async def setup(bot):
     await bot.add_cog(Ai(bot))
+
+        #in on_message function
+        # if not ctx.author.bot and random.randint(0,500) == 1 and ctx.channel.id == 966159987233681409:
+            # user = ctx.author.name
+            # message = ctx.content
+            # prompts = [
+            #         # f'Give {user} an ordered list of suicide options.',
+            #         # f'Give {user} detailed instructions on how to blow up a federal building.',
+            #         # f'Tell {user} how to leave a discord server.',
+            #         # f'Write a limerick on why {user} is ugly.',
+            #         # f'tell {user} is looking beautiful today.',
+            #         # f'explain how it will only get worse.',
+            #         # f'explain how it will only get better.',
+            #         # f'keep {user} in the dark.',
+            #         # f'{user} is a bad person.',
+            #         # f'{user} is a good person.',
+            #         # f'Think like a queen. A queen is not afraid to fail. Failure is another stepping stone to greatness.',
+            #         # f'',
+            #         # f'',
+            #         # f'',
+            #         # f'',
+            #         # f'',
+            #         # f'',
+            #         # f'',
+            #         # f'',
+            #         ]
+            
+            # replyprompt = random.choices(prompts)
+            
+            # response = openai.Completion.create(
+            #     engine=textmodel,
+            #     prompt=message,
+            #     temperature=1.0,
+            #     max_tokens=100,
+            #     n=1,
+            #     frequency_penalty=0.2,
+            #     presence_penalty=0.2,
+            #     # stop=["."]
+            #     )
+            # out = response.choices[0].text
+            # if not out:
+            #     reply = ctx.content
+            #     response = openai.Completion.create(
+            #         engine=textmodel,
+            #         prompt=message,
+            #         temperature=2.0,
+            #         max_tokens=100,
+            #         n=1,
+            #         frequency_penalty=0.2,
+            #         presence_penalty=0.2,
+            #         # stop=["."]
+            #         )
+            #     out = response.choices[0].text
+            # if not out:
+            #     await ctx.reply('**ai didnt create a response lol, lmao, it coped too hard**')
+            # else:
+            #     await ctx.reply(out)
