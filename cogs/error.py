6e24c29d475fde7480e1ca37c5999309b9d0f7aa
SDB-PC <1game2joys@gmail.com>
2023-06-28 15:43:25 +0200

    made it embeds

--- cogs/error.py
+++ cogs/error.py
@@ -6,10 +6,11 @@ from discord.ext import commands
 from configparser import ConfigParser
 
 path = os.path.abspath(os.getcwd())
-config = ConfigParser()
+config, info = ConfigParser(), ConfigParser()
+info.read(rf'{path}/info.ini')
 config.read(rf'{path}/config.ini')
 command_prefix = config['BOTCONFIG']['prefix']
-
+botversion = info['DEFAULT']['title'] + ' v' + info['DEFAULT']['version']
 MSG_DEL_DELAY = 10
 
 class Error(commands.Cog):
@@ -28,25 +29,28 @@ class Error(commands.Cog):
             if hasattr(ctx, '_ignore_'):
                 pass
             else:
-                bot_msg = await ctx.reply("I couldn't send this information to you via direct message. Are your DMs enabled?")
+                embed = discord.Embed(title=f"Error description", description=f"I couldn't send this information to you via direct message. Are your DMs enabled?", color=0xff0000)
+                embed.add_field(name="Error techincal", value=f"{error}", inline=False)
+                embed.set_footer(text=botversion)
                 await ctx.message.delete(delay=MSG_DEL_DELAY)
-                await bot_msg.delete(delay=MSG_DEL_DELAY)
             
         elif isinstance(error, commands.MissingRequiredArgument):
             if hasattr(ctx, '_ignore_'):
                 pass
             else:
-                bot_msg = await ctx.reply(f"You missed the argument `{error.param.name}` for this command!")
+                embed = discord.Embed(title=f"Error description", description=f"You missed the argument `{error.param.name}` for this command!", color=0xff0000)
+                embed.add_field(name="Error techincal", value=f"{error}", inline=False)
+                embed.set_footer(text=botversion)
                 await ctx.message.delete(delay=MSG_DEL_DELAY)
-                await bot_msg.delete(delay=MSG_DEL_DELAY)
         
         elif isinstance(error, commands.MissingPermissions):
             if hasattr(ctx, '_ignore_'):
                 pass
             else:
-                bot_msg = await ctx.reply(f"Missing permissions: {error.missing_permissions}")
+                embed = discord.Embed(title=f"Error description", description=f"Missing permissions: {error.missing_permissions}", color=0xff0000)
+                embed.add_field(name="Error techincal", value=f"{error}", inline=False)
+                embed.set_footer(text=botversion)
                 await ctx.message.delete(delay=MSG_DEL_DELAY)
-                await bot_msg.delete(delay=MSG_DEL_DELAY)
             
         elif isinstance(error, discord.Forbidden):
             if hasattr(ctx, '_ignore_'):
@@ -61,33 +65,41 @@ class Error(commands.Cog):
             if hasattr(ctx, '_ignore_'):
                 pass
             else:
-                bot_msg = await ctx.reply(f"I can't understand this command message! Please check `{command_prefix}help {ctx.command}`")
+                embed = discord.Embed(title=f"Error description", description=f"I can't understand this command message! Please check `{command_prefix}help {ctx.command}`", color=0xff0000)
+                embed.add_field(name="Error techincal", value=f"{error}", inline=False)
+                embed.set_footer(text=botversion)
+                await ctx.reply(embed=embed, mention_author=False, delete_after=MSG_DEL_DELAY)
                 await ctx.message.delete(delay=MSG_DEL_DELAY)
-                await bot_msg.delete(delay=MSG_DEL_DELAY)
             
         elif isinstance(error, commands.CommandNotFound):
             if hasattr(ctx, '_ignore_'):
                 pass
             else:
-                bot_msg = await ctx.reply(f"No such command found. Try `{command_prefix}help`")
+                embed = discord.Embed(title=f"Error description", description=f"No such command found. Try `{command_prefix}help`", color=0xff0000)
+                embed.add_field(name="Error techincal", value=f"{error}", inline=False)
+                embed.set_footer(text=botversion)
+                await ctx.reply(embed=embed, mention_author=False, delete_after=MSG_DEL_DELAY)
                 await ctx.message.delete(delay=MSG_DEL_DELAY)
-                await bot_msg.delete(delay=MSG_DEL_DELAY)
             
         elif isinstance(error, commands.CommandOnCooldown):
             if hasattr(ctx, '_ignore_'):
                 pass
             else:
-                bot_msg = await ctx.reply(f"This command is on cooldown retard. Try again in {error.retry_after:.2f} seconds.")
+                embed = discord.Embed(title=f"Error description", description=f"This command is on cooldown bozo. Try again in {error.retry_after:.2f} seconds.")
+                embed.add_field(name="Error technical", value=f"{error}", inline=False)
+                embed.set_footer(text=botversion)
+                await ctx.reply(embed=embed, mention_author=False, delete_after=MSG_DEL_DELAY)
                 await ctx.message.delete(delay=MSG_DEL_DELAY)
-                await bot_msg.delete(delay=MSG_DEL_DELAY)
         
         elif isinstance(error, commands.CommandError):
             if hasattr(ctx, '_ignore_'):
                 pass
             else:
-                bot_msg = await ctx.reply("The command you've entered could not be completed at this time.")
+                embed = discord.Embed(name="Error description", description=f"The command you've entered could not be completed at this time.")
+                embed.add_field(name="Error technical", value=f"{error}", inline=False)
+                embed.set_footer(text=botversion)
+                await ctx.reply(embed=embed, mention_author=False, delete_after=MSG_DEL_DELAY)
                 await ctx.message.delete(delay=MSG_DEL_DELAY)
-                await bot_msg.delete(delay=MSG_DEL_DELAY)
                 print(f'CONSOLE ONLY, COMMAND FAILED: {error}')        
         else:            
             print(f'CONSOLE ONLY, ALL FAILED: {error} and {traceback.format_exc()}')
