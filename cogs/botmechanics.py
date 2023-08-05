74562dfb0a29c08f20e022af5c561ed2abbbe509
SDB-PC <1game2joys@gmail.com>
2023-08-05 22:34:07 +0200

    rename

--- cogs/botmechanics.py
+++ cogs/botmechanics.py
@@ -1,23 +1,52 @@
 import sys
 import os
+import git
 from discord.ext import commands
 from configparser import ConfigParser
 
-path = os.path.abspath(os.getcwd())
+ospath = os.path.abspath(os.getcwd())
 config = ConfigParser()
-config.read(rf'{path}/config.ini')
+config.read(rf'{ospath}/config.ini')
 owner_id = int(config['BOTCONFIG']['ownerid'])
 
 MSG_DEL_DELAY = 10
 
-class CogManager(commands.Cog):
-    '''This Cogs module is used to load or unload different modules to update the bot without having to take it offline'''
+class BotMechanics(commands.Cog):
+    '''This module is used to load or unload different modules to update the bot without having to take it offline'''
     def __init__(self, bot):
         self.bot = bot
     
     @commands.Cog.listener()
     async def on_ready(self):
         print('Cog loader/unloader/reloader module online')
+    
+    @commands.command(aliases=["update"], hidden=True)
+    async def update_bot(self, ctx):
+        '''
+        Update the bot's code from the remote repository and report the number of commits behind and ahead. 
+        If the bot is already up to date, no action is taken. 
+        This command is hidden and can only be used by the bot owner.
+        '''
+        if ctx.author.id == owner_id:
+            repo = git.Repo(ospath)
+            repo.remotes.origin.fetch()
+            commits_behind = len(list(repo.iter_commits('master..origin/master')))
+            commits_ahead = len(list(repo.iter_commits('origin/master..master')))
+            await ctx.send(f'{commits_behind} commits behind, {commits_ahead} commits ahead', delete_after=MSG_DEL_DELAY)
+            
+            if commits_behind > 0 and commits_ahead > 0:
+                repo.remotes.origin.push()
+                repo.remotes.origin.pull()
+                await ctx.send('KirBot local and remote updated', delete_after=MSG_DEL_DELAY)
+            elif commits_behind > 0:
+                repo.remotes.origin.pull()
+                await ctx.send('KirBot local updated', delete_after=MSG_DEL_DELAY)	
+            elif commits_ahead > 0:
+                repo.remotes.origin.push()
+                await ctx.send('KirBot remote updated', delete_after=MSG_DEL_DELAY)
+            else:
+                await ctx.send('KirkBot is already up to date with the remote', delete_after=MSG_DEL_DELAY)
+        await ctx.message.delete(delay=MSG_DEL_DELAY)
     
     @commands.command(hidden=True)
     @commands.has_permissions(administrator=True)
@@ -70,7 +99,7 @@ class CogManager(commands.Cog):
         '''Reloads all modules.'''
         if ctx.author.id == owner_id:
             msg = await ctx.send("Trying to reload all modules...")
-            for filename in os.listdir(os.path.abspath(os.getcwd()) + '/cogs'):
+            for filename in os.listdir(rf'{ospath}/cogs'):
                 if filename.endswith(".py"):
                     name = filename[:-3]
                     try:
@@ -92,7 +121,7 @@ class CogManager(commands.Cog):
         '''Loads all modules.'''
         if ctx.author.id == owner_id:
             msg = await ctx.send("Trying to load all modules...")
-            for filename in os.listdir(os.path.abspath(os.getcwd()) + '/cogs'):
+            for filename in os.listdir(rf'{ospath}/cogs'):
                 if filename.endswith(".py"):
                     name = filename[:-3]
                     try:
@@ -113,7 +142,7 @@ class CogManager(commands.Cog):
         '''Unloads all modules.'''
         if ctx.author.id == owner_id:
             msg = await ctx.send(f"Unloading modules")
-            for filename in os.listdir(os.path.abspath(os.getcwd()) + '/cogs'):
+            for filename in os.listdir(rf'{ospath}/cogs'):
                 if filename.endswith(".py"):
                     name = filename[:-3]
                     try:
@@ -133,4 +162,4 @@ class CogManager(commands.Cog):
             await ctx.message.delete(delay=MSG_DEL_DELAY)
 
 async def setup(bot):
-    await bot.add_cog(CogManager(bot))
+    await bot.add_cog(BotMechanics(bot))
