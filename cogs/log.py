2997c59938f882d525630475b2e1e6c54bfabfff
SDB-PC <1game2joys@gmail.com>
2023-10-30 16:01:40 +0100

    building

--- cogs/log.py
+++ cogs/log.py
@@ -5,8 +5,11 @@ from discord.ext import commands
 class Log(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        
 
-    
+    @commands.Cog.listener()
+    async def on_a(self, message):
+        ...
 
 async def setup(bot):
     await bot.add_cog(Log(bot))
