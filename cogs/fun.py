20304d34d651b3fc0955287d5473a37e72373352
SDB-PC <1game2joys@gmail.com>
2024-04-15 00:00:59 +0200

    Update window size in fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -355,7 +355,7 @@ class Fun(commands.Cog):
             
             # options.add_argument("--headless")
             options.add_argument("--headless=new")
-            options.add_argument("--window-size=1000,500")
+            options.add_argument("--window-size=2000,1000")
             options.add_argument("--disable-gpu")
             options.add_argument("--disable-extensions")
             
