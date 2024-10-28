f0ab7e3c51f82df69b42f97013722f02799e9296
SDB-PC <1game2joys@gmail.com>
2024-10-28 20:20:35 +0100

    Refactor StatusManager: Replace status updater loop with a new switch status loop and improve task management

--- cogs/botstatus.py
+++ cogs/botstatus.py
@@ -18,17 +18,18 @@ status = info['STATUS']['status']
 activity = discord.Activity(name=status, type=discord.ActivityType.watching)
 started_tasks = []
 
+SECOND_LOOP_DELAY = 10
+
 class StatusManager(commands.Cog):
     '''
     Manages the bot's status.
     '''
     def __init__(self, bot):
         self.bot = bot
-        self.status_updater.start()
+        self.switch_status_task = None
+        if not self.switch_status_task or self.switch_status_task.done():
+            self.switch_status_task = asyncio.create_task(self.switch_status_loop())
     
-    def cog_unload(self):
-        self.status_updater.cancel()
-
     #events
     @commands.Cog.listener()
     async def on_ready(self):
@@ -55,7 +56,7 @@ class StatusManager(commands.Cog):
         message = await ctx.send(f"[1️⃣ for watching]. [2️⃣ for listening to.]")
         await message.add_reaction('1️⃣')
         await message.add_reaction('2️⃣')
-
+        
         check = lambda r, u: u == ctx.author and str(r.emoji) in "1️⃣2️⃣"
         try:
             reaction, user = await self.bot.wait_for("reaction_add", check=check, timeout=10)
@@ -64,45 +65,64 @@ class StatusManager(commands.Cog):
             await ctx.message.delete()
             await message.edit(content="update cancelled, timed out.")
             return
-
+        
         if str(reaction.emoji) == "1️⃣":
             activity = discord.Activity(name=statusmessage, type=discord.ActivityType.watching)
             await self.bot.change_presence(status=discord.Status.online, activity=activity)
             strstatus = 'Watching'
-
+        
         elif str(reaction.emoji) == "2️⃣":
             activity = discord.Activity(name=statusmessage, type=discord.ActivityType.listening)
             await self.bot.change_presence(status=discord.Status.online, activity=activity)
             strstatus = 'Listening to'
-
+        
         else:
             await message.edit(content="Update cancelled.")
             return
         
         info.set('STATUS', 'status', statusmessage)
-
+        
         with open(rf'{ospath}/info.ini', 'w') as infofile:
             info.write(infofile)
         await message.edit(content=f'Updated Status to: {strstatus} {statusmessage}')
         await message.clear_reactions()
     
-    @tasks.loop()
-    async def status_updater(self):
-        activity = discord.Activity(name=status, type=discord.ActivityType.watching)
-        await self.bot.change_presence(status=discord.Status.online, activity=activity)
-        await asyncio.sleep(10)
-        
-        activity = discord.Activity(name=f'{command_prefix}help', type=discord.ActivityType.playing)
-        await self.bot.change_presence(status=discord.Status.online, activity=activity)
-        await asyncio.sleep(10)
+    async def cog_unload(self):
+        # print('Autodelete cog_unload stopping switch_status_loop')
+        if self.switch_status_task and not self.switch_status_task.done():
+            self.switch_status_task.cancel()
     
-    @status_updater.after_loop
-    async def after_status_updater(self):
-        pass
+    async def cog_load(self):
+        await asyncio.sleep(2)
+        # print('Autodelete cog_load starting switch_status_loop')
+        if self.switch_status_task and self.switch_status_task.done():
+            loop = asyncio.get_event_loop()
+            self.switch_status_task = loop.create_task(self.switch_status_loop())
+            await self.fetch_missed_messages()
     
-    @status_updater.before_loop
-    async def before_status_updater(self):
-        await self.bot.wait_until_ready()
-
+    @commands.Cog.listener()
+    async def on_disconnect(self):
+        if self.switch_status_task and not self.switch_status_task.done():
+            self.switch_status_task.cancel()
+    
+    @commands.Cog.listener()
+    async def on_resumed(self):
+        await asyncio.sleep(2)
+        if self.switch_status_task and self.switch_status_task.done():
+            loop = asyncio.get_event_loop()
+            self.switch_status_task = loop.create_task(self.switch_status_loop())
+            await self.fetch_missed_messages()
+    
+    async def switch_status_loop(self):
+        while True:
+            # print ('switch_status_loop')
+            activity = discord.Activity(name=status, type=discord.ActivityType.watching)
+            await self.bot.change_presence(status=discord.Status.online, activity=activity)
+            await asyncio.sleep(SECOND_LOOP_DELAY)
+            
+            activity = discord.Activity(name=f'{command_prefix}help', type=discord.ActivityType.playing)
+            await self.bot.change_presence(status=discord.Status.online, activity=activity)
+            await asyncio.sleep(SECOND_LOOP_DELAY)
+    
 async def setup(bot):
     await bot.add_cog(StatusManager(bot))
