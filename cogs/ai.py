6a7ce97800dbab621e2221b23583aaad5706bde8
SDB-PC <1game2joys@gmail.com>
2026-03-24 18:34:20 +0100

    Update text model to 'llama2-uncensored' for consistency

--- cogs/ai.py
+++ cogs/ai.py
@@ -13,7 +13,7 @@ from cogs.utils.constants import (ARCHIVE_DATABASE, BOTID, MEME_WORD_COUNT,
                                   ORDER, PERMISSIONS_DATABASE, TEXT_WORD_COUNT,
                                   MSG_DEL_DELAY)
 
-textmodel = 'llama2-uncensored:7b'
+textmodel = 'llama2-uncensored'
 
 class MarkovChain:
     def __init__(self, order:int):
